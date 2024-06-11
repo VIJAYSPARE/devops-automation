bf12c701137db5e37c2cdcac438b03d4bee66674
VIJAYSPARE <95615213+VIJAYSPARE@users.noreply.github.com>
2024-06-11 15:10:23 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "Welcome to Vijays DevOps";
+		return "welcome to javatechie";
 	}
 
 	public static void main(String[] args) {
